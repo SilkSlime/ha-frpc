a3ed423eaec2f7b64493e6720b05ff99d72fab6b
a.panfilkin <a.panfilkin@iva.ru>
2025-05-05 02:40:34 +0300

    fix:

--- frp_client/run.sh
+++ frp_client/run.sh
@@ -28,34 +28,40 @@ tls.trustedCaFile = "$(bashio::config 'tlsCaFile')"
 EOF
 fi
 
-# === Proxies ===
+# === Proxies section ===
 bashio::log.info "▶ Appending proxies"
 
-for i in $(bashio::config 'proxies|keys'); do
-  name=$(bashio::config "proxies[${i}].name")
-  type=$(bashio::config "proxies[${i}].type")
-  localIP=$(bashio::config "proxies[${i}].localIP")
-  localPort=$(bashio::config "proxies[${i}].localPort")
-  remotePort=$(bashio::config "proxies[${i}].remotePort")
-  customDomains=$(bashio::config "proxies[${i}].customDomains")
-  useEnc=$(bashio::config "proxies[${i}].useEncryption")
-  useComp=$(bashio::config "proxies[${i}].useCompression")
+if bashio::config.has_value 'proxies'; then
+  for i in $(bashio::config 'proxies|keys'); do
+    name=$(bashio::config "proxies[${i}].name")
+    type=$(bashio::config "proxies[${i}].type")
+    localIP=$(bashio::config "proxies[${i}].localIP")
+    localPort=$(bashio::config "proxies[${i}].localPort")
+    remotePort=$(bashio::config "proxies[${i}].remotePort")
+    useEnc=$(bashio::config "proxies[${i}].useEncryption")
+    useComp=$(bashio::config "proxies[${i}].useCompression")
 
-  echo -e "\n[[proxies]]" >>"$CONFIG_PATH"
-  echo "name = \"$name\"" >>"$CONFIG_PATH"
-  echo "type = \"$type\"" >>"$CONFIG_PATH"
-  echo "localIP = \"$localIP\"" >>"$CONFIG_PATH"
-  echo "localPort = $localPort" >>"$CONFIG_PATH"
-  echo "remotePort = $remotePort" >>"$CONFIG_PATH"
+    echo -e "\n[[proxies]]" >>"$CONFIG_PATH"
+    echo "name = \"$name\"" >>"$CONFIG_PATH"
+    echo "type = \"$type\"" >>"$CONFIG_PATH"
+    echo "localIP = \"$localIP\"" >>"$CONFIG_PATH"
+    echo "localPort = $localPort" >>"$CONFIG_PATH"
+    echo "remotePort = $remotePort" >>"$CONFIG_PATH"
 
-  if bashio::config.has_value "proxies[${i}].customDomains"; then
-    echo "customDomains = [\"$customDomains\"]" >>"$CONFIG_PATH"
-  fi
+    # customDomains: list of strings
+    if bashio::config.has_value "proxies[${i}].customDomains"; then
+      domains=$(bashio::config "proxies[${i}].customDomains" | jq -r '. | map("\""+.+"\"") | join(", ")')
+      echo "customDomains = [${domains}]" >>"$CONFIG_PATH"
+    fi
 
-  echo "transport.useEncryption = $useEnc" >>"$CONFIG_PATH"
-  echo "transport.useCompression = $useComp" >>"$CONFIG_PATH"
-done
+    echo "transport.useEncryption = $useEnc" >>"$CONFIG_PATH"
+    echo "transport.useCompression = $useComp" >>"$CONFIG_PATH"
+  done
+fi
 
 # === Start FRPC ===
+bashio::log.info "▶ Final generated config:"
+cat "$CONFIG_PATH"
+
 bashio::log.info "▶ Starting FRPC client"
 /usr/src/frpc -c "$CONFIG_PATH"
