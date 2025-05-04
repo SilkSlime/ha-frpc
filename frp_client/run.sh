91840d3609d39114d64fad198591cdad9fd4b9cd
a.panfilkin <a.panfilkin@iva.ru>
2025-05-05 00:53:46 +0300

    fix: raw config

--- frp_client/run.sh
+++ frp_client/run.sh
@@ -3,37 +3,35 @@
 CONFIG_PATH="/share/frpc.toml"
 bashio::log.info "▶ Generating FRPC config"
 
-# [common] section
+# common section
 cat <<EOF >"$CONFIG_PATH"
 [common]
-serverAddr   = "$(bashio::config 'serverAddr')"
-serverPort   = $(bashio::config 'serverPort')
-auth.method  = "$(bashio::config 'authMethod')"
-auth.token   = "$(bashio::config 'authToken')"
-log.to       = "/share/frpc.log"
-log.level    = "info"
-log.maxDays  = 3
+serverAddr  = "$(bashio::config 'serverAddr')"
+serverPort  = $(bashio::config 'serverPort')
+auth.method = "$(bashio::config 'authMethod')"
+auth.token  = "$(bashio::config 'authToken')"
+log.to      = "/share/frpc.log"
+log.level   = "info"
+log.maxDays = 3
 EOF
 
-# TLS block
+# TLS
 if bashio::config.true 'tlsEnable'; then
   cat <<EOF >>"$CONFIG_PATH"
-tls.enable          = true
-tls.certFile        = "$(bashio::config 'tlsCertFile')"
-tls.keyFile         = "$(bashio::config 'tlsKeyFile')"
-tls.trustedCaFile   = "$(bashio::config 'tlsCaFile')"
+tls.enable        = true
+tls.certFile      = "$(bashio::config 'tlsCertFile')"
+tls.keyFile       = "$(bashio::config 'tlsKeyFile')"
+tls.trustedCaFile = "$(bashio::config 'tlsCaFile')"
 EOF
 fi
 
-# Append user-defined proxies
-bashio::log.info "▶ Appending user proxies"
+# user proxies
+bashio::log.info "▶ Appending [[proxies]]"
 echo "" >>"$CONFIG_PATH"
-cat <<EOF >>"$CONFIG_PATH"
-$(bashio::config 'frpcConfig')
-EOF
+bashio::config 'frpcConfig' >>"$CONFIG_PATH"
 
-# Start FRP client
-bashio::log.info "▶ Starting FRP client"
-trap 'bashio::log.info "⏹ Stopping FRP client"; kill 0' SIGINT SIGTERM
+# start client
+bashio::log.info "▶ Starting FRPC"
+trap 'bashio::log.info "⏹ Stopping FRPC"; kill 0' SIGINT SIGTERM
 /usr/src/frpc -c "$CONFIG_PATH" &
 wait
